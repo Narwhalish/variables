a45281ba09211786379fe8c368ddc21455ec928e
Narwhalish <bryao@seas.upenn.edu>
2022-10-11 15:32:26 -0400

    Add example for changing mutable variable.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-    let x = 5;
+    // changing immutable variable example
+    // let x = 5;
+    // println!("Value of x is {x}");
+    // x = 6;
+    // println("New value of x is {x}");
+
+    // changing mutable variable example
+    let mut x = 5;
     println!("Value of x is {x}");
-    x = 6;
-    println("New value of x is {x}");
+    x = 70;
+    println!("New value of x is {x}");
 }
